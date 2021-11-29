452cea56299929725efcca91fb00677f73721866
unknown <ngotienpts@gmail.com>
2021-11-29 17:31:38 +0700

    29/11

--- js/main.js
+++ js/main.js
@@ -3,6 +3,21 @@ document.addEventListener("DOMContentLoaded", function () {
     // get height header
     var heightHeader = document.querySelector('#header');
     var main = document.querySelector('#main');
+
+    // login
+    var openLogin = document.querySelectorAll('.header-right-item__login');
+    var popupLogin = document.querySelector('.login-wrapper');
+    var loginField = document.querySelector('.login-field');
+    var closeLogin = document.querySelector('.login-close');
+    var changeRegister = document.querySelector('.register-now');
+
+    // register
+    var openRegister = document.querySelectorAll('.header-right-item__register');
+    var popupRegister = document.querySelector('.register-wrapper');
+    var registerField = document.querySelector('.register-field');
+    var closeRegister = document.querySelector('.register-close');
+    var changeLogin = document.querySelector('.login-now');
+
     const app = {
       // get height cua header day main theo chieu cao cua header
       pushMain:function(){
@@ -16,6 +31,65 @@ document.addEventListener("DOMContentLoaded", function () {
       handleEvent: function () {
         const _this = this;
         
+        // show login form
+        if(openLogin){
+          openLogin.forEach(function(login){
+            login.onclick = function(){
+              if(popupLogin.classList.contains('open')){
+                popupLogin.classList.remove('open')
+              }else {
+                popupLogin.classList.add('open');
+                popupRegister.classList.remove('open');
+              }
+            }
+          })
+          // close login form
+          if(closeLogin){
+            closeLogin.onclick = function(){
+              if(popupLogin){
+                popupLogin.classList.remove('open')
+              }
+            }
+          }
+          //change register
+          if(changeRegister){
+            changeRegister.onclick = function(){
+              popupLogin.classList.remove('open');
+              popupRegister.classList.add('open');
+            }
+          }
+        }
+
+        // show register form
+        if(openRegister){
+          openRegister.forEach(function(register){
+            register.onclick = function(){
+              if(popupRegister.classList.contains('open')){
+                popupRegister.classList.remove('open')
+              }else {
+                popupRegister.classList.add('open');
+                popupLogin.classList.remove('open');
+              }
+            }
+          })
+          // close login form
+          if(closeRegister){
+            closeRegister.onclick = function(){
+              if(popupRegister){
+                popupRegister.classList.remove('open')
+              }
+            }
+          }
+          //change login
+          if(changeLogin){
+            changeLogin.onclick = function(){
+              popupRegister.classList.remove('open');
+              popupLogin.classList.add('open');
+            }
+          }
+        }
+
+
         // hide cac element khi click ra ngoai
         document.addEventListener('click',function(e){
           
